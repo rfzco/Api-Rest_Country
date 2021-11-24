0ecd9ff5d1b186244706eaccf11ac3974637e4d2
rfzco <rafalorozco77@gmail.com>
2021-11-23 20:32:46 -0400

    Entrega

--- Js/dark.mode.js
+++ Js/dark.mode.js
@@ -1,7 +1,7 @@
 const btnDark = document.querySelector('.btn-dark-mode');
 
 btnDark.addEventListener('click', () => {
-    console.log('diste click')
+
     document.body.classList.toggle('dark-mode');
 
     if (document.body.className === 'dark-mode') {
--- Js/home.js
+++ Js/home.js
@@ -10,44 +10,49 @@ const fetchData = async () => {
         const data = await res.json()
         banderillas(data)
         formularioCliente(data)
-        filtros(data)
     } catch (error) {
         console.log(error)
     }
 }
 
+
 const banderillas = data => {
-    let elementos = ''
-    data.forEach(item => {
+    let elements = '';
+    data.forEach(item => { 
         
-        elementos += 
-        
-        `
-        <article class="card">
-            <img src="${item.flag}" alt="" class="img-fluid">
-            <div class="card-content">
-                <h3>${item.name}</h3>
-                <p>
-                    <b>Population: </b>
-                    ${item.population}
-                </p>
-                <p>
-                    <b>Capital: </b>
-                    ${item.capital}
-                </p>
-                <p>
-                    <b>Región: </b>
-                    ${item.region}
-                </p>
-                <p>
-                    <a href="nuevo-pais.html?name=${item.name}">Más info</a>
-                </p>
-            </div>
+
+
+        elements +=
+
+
+        `<article class="card">
+             <img src="${item.flag}" alt="" class="img-fluid">
+             <div class="card-content">
+                 <h3>${item.name}</h3>
+                 <p>
+                     <b>Population: </b>
+                     ${item.population}
+                 </p>
+                 <p>
+                     <b>Capital: </b>
+                     ${item.capital}
+                 </p>
+                 <p>
+                     <b>Región: </b>
+                     ${item.region}
+                 </p>
+                 <p>
+                     <a href="nuevo-pais.html?name=${item.name}" class="ov-btn-slide-left">More info</a>
+                 </p>
+             </div>
         </article>`   
-        
-     
-});
-    banderas.innerHTML = elementos
+             
+      });
+    banderas.innerHTML = elements
     
-    
-}
+   
+}    
+
+
+
+ 
